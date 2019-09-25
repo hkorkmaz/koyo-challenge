21b6b6a56fcb6cd12ee973711b4da5e9ad0fce35
Habib Korkmaz <habib.korkmaz@sony.com>
2019-09-25 13:52:34 +0300

    Minor refactoring

--- src/main/kotlin/com/chess/Board.kt
+++ src/main/kotlin/com/chess/Board.kt
@@ -15,49 +15,7 @@ class Board {
     private val pieces = mutableMapOf<Square, Piece>()
 
     init {
-        putPawns()
-        putOtherPieces()
-    }
-
-    private fun putPawns() {
-        val a2 = Square("A2")
-        for (i in 0 until BOARD_SIZE) {
-            val pawnWhite = Pawn(a2.right(i), Color.WHITE, this)
-            val pawnBlack = Pawn(a2.right(i).symmetricX(), Color.BLACK, this)
-            pieces[a2.right(i)] = pawnWhite
-            pieces[a2.right(i).symmetricX()] = pawnBlack
-        }
-    }
-
-    private fun putOtherPieces() {
-        val c1 = Square("C1")
-        val f1 = Square("F1")
-        pieces[c1] = Bishop(c1, Color.WHITE, this)
-        pieces[f1] = Bishop(f1, Color.WHITE, this)
-        pieces[c1.symmetricX()] = Bishop(c1.symmetricX(), Color.BLACK, this)
-        pieces[f1.symmetricX()] = Bishop(f1.symmetricX(), Color.BLACK, this)
-
-        val b1 = Square("B1")
-        val g1 = Square("G1")
-        pieces[b1] = Knight(b1, Color.WHITE, this)
-        pieces[g1] = Knight(g1, Color.WHITE, this)
-        pieces[b1.symmetricX()] = Knight(b1.symmetricX(), Color.BLACK, this)
-        pieces[g1.symmetricX()] = Knight(g1.symmetricX(), Color.BLACK, this)
-
-        val a1 = Square("A1")
-        val h1 = Square("H1")
-        pieces[a1] = Rook(a1, Color.WHITE, this)
-        pieces[h1] = Rook(h1, Color.WHITE, this)
-        pieces[a1.symmetricX()] = Rook(a1.symmetricX(), Color.BLACK, this)
-        pieces[h1.symmetricX()] = Rook(h1.symmetricX(), Color.BLACK, this)
-
-        val e1 = Square("E1")
-        pieces[e1] = King(e1, Color.WHITE, this)
-        pieces[e1.symmetricX()] = King(e1.symmetricX(), Color.BLACK, this)
-
-        val d1 = Square("D1")
-        pieces[d1] = Queen(d1, Color.WHITE, this)
-        pieces[d1.symmetricX()] = Queen(d1.symmetricX(), Color.BLACK, this)
+        putPieces()
     }
 
     fun move(player: Player, sourceSquare: Square, targetSquare: Square) {
@@ -90,25 +48,29 @@ class Board {
     }
 
     override fun toString(): String {
+        val pipe = "|"
+        val dash = "-"
+        val space = " "
         val s = StringBuilder()
-        s.append("=".padEnd(90, '=')).appendln()
+
+        s.append(dash.repeat(90)).appendln()
 
         for (y in BOARD_END_Y downTo BOARD_START_Y) {
-            s.append(y).append(" ")
+            s.append(y).append(space)
 
             for (x in BOARD_START_X..BOARD_END_X) {
-                val square = Square("$x$y")
-                val piece = pieces[square] ?: " - ".padEnd(10, ' ')
-                s.append("|$piece")
+                val piece = pieces[Square("$x$y")]
+                if(piece == null)
+                    s.append(pipe).append(dash.padEnd(10, ' '))
+                else
+                    s.append(pipe).append(piece)
             }
-            s.append("|").appendln()
+            s.append(pipe).appendln()
         }
-        s.append("-".padEnd(90, '-'))
-            .appendln()
-            .append("  |")
+        s.append(dash.repeat(90)).appendln().append(space.repeat(2)).append(pipe)
 
         for (x in BOARD_START_X..BOARD_END_X) {
-            s.append(" $x".padEnd(10, ' ')).append("|")
+            s.append("$x".padEnd(10, ' ')).append(pipe)
         }
 
         return s.appendln().toString()
@@ -130,5 +92,50 @@ class Board {
 
     private fun isEmpty(position: Square): Boolean {
         return pieces[position] == null
+    }
+
+    private fun putPieces() {
+        // Pawns
+        val a2 = Square("A2")
+        for (i in 0 until BOARD_SIZE) {
+            val pawnWhite = Pawn(a2.right(i), Color.WHITE, this)
+            val pawnBlack = Pawn(a2.right(i).symmetricX(), Color.BLACK, this)
+            pieces[a2.right(i)] = pawnWhite
+            pieces[a2.right(i).symmetricX()] = pawnBlack
+        }
+
+        // Bishops
+        val c1 = Square("C1")
+        val f1 = Square("F1")
+        pieces[c1] = Bishop(c1, Color.WHITE, this)
+        pieces[f1] = Bishop(f1, Color.WHITE, this)
+        pieces[c1.symmetricX()] = Bishop(c1.symmetricX(), Color.BLACK, this)
+        pieces[f1.symmetricX()] = Bishop(f1.symmetricX(), Color.BLACK, this)
+
+        // Knights
+        val b1 = Square("B1")
+        val g1 = Square("G1")
+        pieces[b1] = Knight(b1, Color.WHITE, this)
+        pieces[g1] = Knight(g1, Color.WHITE, this)
+        pieces[b1.symmetricX()] = Knight(b1.symmetricX(), Color.BLACK, this)
+        pieces[g1.symmetricX()] = Knight(g1.symmetricX(), Color.BLACK, this)
+
+        // Rooks
+        val a1 = Square("A1")
+        val h1 = Square("H1")
+        pieces[a1] = Rook(a1, Color.WHITE, this)
+        pieces[h1] = Rook(h1, Color.WHITE, this)
+        pieces[a1.symmetricX()] = Rook(a1.symmetricX(), Color.BLACK, this)
+        pieces[h1.symmetricX()] = Rook(h1.symmetricX(), Color.BLACK, this)
+
+        // Kings
+        val e1 = Square("E1")
+        pieces[e1] = King(e1, Color.WHITE, this)
+        pieces[e1.symmetricX()] = King(e1.symmetricX(), Color.BLACK, this)
+
+        // Queens
+        val d1 = Square("D1")
+        pieces[d1] = Queen(d1, Color.WHITE, this)
+        pieces[d1.symmetricX()] = Queen(d1.symmetricX(), Color.BLACK, this)
     }
 }
